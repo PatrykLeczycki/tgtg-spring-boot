4daeaa8cf70d2a27000437904016b0bc91be73b9
Patryk <patryk.leczycki1@gmail.com>
2020-11-26 14:10:28 +0100

    added DTO-entity model mapper

---
+++ src/main/java/com/pleczycki/tgtg/utils/CustomModelMapper.java
@@ -0,0 +1,8 @@
+package com.pleczycki.tgtg.utils;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomModelMapper extends ModelMapper {
+}
