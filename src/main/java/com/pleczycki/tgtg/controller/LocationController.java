000b0685db0ab40e45fd8ef673e7e7aceb8aa629
pleczycki <pleczycki@eversis.com>
2020-12-01 18:53:41 +0100

    code cleanup and refactoring for easier testing, added unit tests

--- src/main/java/com/pleczycki/tgtg/controller/LocationController.java
+++ src/main/java/com/pleczycki/tgtg/controller/LocationController.java
@@ -1,18 +1,14 @@
 package com.pleczycki.tgtg.controller;
 
 import com.pleczycki.tgtg.dto.LocationDto;
-import com.pleczycki.tgtg.dto.ReviewDto;
 import com.pleczycki.tgtg.model.Location;
-import com.pleczycki.tgtg.model.Review;
 import com.pleczycki.tgtg.service.LocationService;
-import com.pleczycki.tgtg.utils.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-import java.util.Map;
 import javax.validation.Valid;
 
 @RestController
@@ -24,38 +20,38 @@ public class LocationController {
     private LocationService locationService;
 
     @PostMapping("/add")
-    public ApiResponse addLocation(@Valid @RequestBody LocationDto locationDto) {
+    public ResponseEntity<Location> addLocation(@Valid @RequestBody LocationDto locationDto) {
         return locationService.addLocation(locationDto);
     }
 
     @PostMapping("/edit/{id}")
-    public ApiResponse updateReview(@RequestBody LocationDto locationDto,
+    public ResponseEntity<Location> updateReview(@RequestBody LocationDto locationDto,
                                     @PathVariable("id") Long locationId) {
         return locationService.updateLocation(locationDto, locationId);
     }
 
     @GetMapping("/all")
-    public List<Location> getAll() {
+    public ResponseEntity<List<Location>> getAll() {
         return locationService.getAll();
     }
 
     @GetMapping("/map")
-    public List<Location> getMap() {
+    public ResponseEntity<List<Location>> getMap() {
         return locationService.getAll();
     }
 
     @GetMapping("/map/latest")
-    public List<Location> getLatestInCities() {
+    public ResponseEntity<List<Location>> getLatestInCities() {
         return locationService.getLatestInCities();
     }
 
     @GetMapping("/all/{city}")
-    public List<Location> getAllByCity(@PathVariable String city) {
+    public ResponseEntity<List<Location>> getAllByCity(@PathVariable String city) {
         return locationService.getAllByCity(city);
     }
 
     @GetMapping("/get/{id}")
-    public Location getReview(@PathVariable Long id) {
+    public ResponseEntity<Location> getReview(@PathVariable Long id) {
         return locationService.getLocation(id);
     }
 }
--- src/main/java/com/pleczycki/tgtg/controller/ReviewController.java
+++ src/main/java/com/pleczycki/tgtg/controller/ReviewController.java
@@ -3,9 +3,10 @@ package com.pleczycki.tgtg.controller;
 import com.pleczycki.tgtg.dto.ReviewDto;
 import com.pleczycki.tgtg.model.Review;
 import com.pleczycki.tgtg.service.ReviewService;
-import com.pleczycki.tgtg.utils.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,46 +22,47 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/review")
+@Slf4j
 public class ReviewController {
 
     @Autowired
     private ReviewService reviewService;
 
     @PostMapping("/add")
-    public ApiResponse addReview(@RequestPart("review") ReviewDto reviewDto, @RequestPart("id") Long userId,
+    public ResponseEntity<Review> addReview(@RequestPart("review") ReviewDto reviewDto, @RequestPart("id") Long userId,
                                  @RequestPart("files") List<MultipartFile> files) {
         return reviewService.addReview(reviewDto, userId, files);
     }
 
     @PostMapping("/edit/{id}")
-    public ApiResponse updateReview(@RequestPart("review") ReviewDto reviewDto,
+    public ResponseEntity<Review> updateReview(@RequestPart("review") ReviewDto reviewDto,
                                     @RequestPart("files") List<MultipartFile> files, @RequestPart("deleted") List<String> deletedPhotosIds,
                                     @PathVariable("id") Long reviewId) {
         return reviewService.updateReview(reviewDto, reviewId, files, deletedPhotosIds);
     }
 
     @GetMapping("/all")
-    public List<Review> getAllReviews() {
+    public ResponseEntity<List<Review>> getAllReviews() {
         return reviewService.getAllReviews();
     }
 
     @GetMapping("/all/user/{id}")
-    public List<Review> getAllReviewsByUserId(@PathVariable("id") Long id) {
+    public ResponseEntity<List<Review>> getAllReviewsByUserId(@PathVariable("id") Long id) {
         return reviewService.getAllReviewsByUserId(id);
     }
 
     @GetMapping("/latest")
-    public List<Review> getLatestReviews(@RequestParam int limit) {
+    public ResponseEntity<List<Review>> getLatestReviews(@RequestParam int limit) {
         return reviewService.getLatestReviews(limit);
     }
 
     @GetMapping("/location/{id}/latest")
-    public List<Review> getLatestLocationReviews(@PathVariable("id") long locationId) {
+    public ResponseEntity<List<Review>> getLatestLocationReviews(@PathVariable("id") long locationId) {
         return reviewService.getLatestLocationReviews(locationId);
     }
 
     @GetMapping("/get/{id}")
-    public Review getReview(@PathVariable Long id) {
+    public ResponseEntity<Review> getReview(@PathVariable Long id) {
         return reviewService.getReview(id);
     }
 
